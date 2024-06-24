e265dbaff56f086ccda8e41874744363c48cd36b
Gopal1252 <guptagopal252@gmail.com>
2024-06-24 22:16:18 +0530

    added jest-svg-transformer and made changes to jest config to make compatible with svg

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import Header from "../Header";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+it("Should load Header Component with a login button", () => {
+    render(
+        <BrowserRouter>
+            <Provider store={appStore}>
+                <Header/>
+            </Provider>
+        </BrowserRouter>
+    );
+
+    const loginButton = screen.getByRole("button");
+
+    expect(loginButton).toBeInTheDocument();
+});
